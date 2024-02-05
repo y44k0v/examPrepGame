e85481bdc1054db58e02dd28a82992085924e505
y44k0v <ykvgm001@gmail.com>
2024-02-05 17:36:14 -0500

    file tracking added

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/y44k0v/examPrepGame
+
+go 1.21.6
---
+++ main.go
@@ -0,0 +1,56 @@
+/*
+Lab Assignment: Building an Exam Preparation Game 
+Given on Friday 020224
+By Yaakov Miller
+*/
+package main
+
+import ("fmt",
+		"math/rand",
+		"time")
+
+func main() {
+	
+	// different stages of exam preparation
+	stages := []string {
+
+	}
+	// preparation activities
+	activities := [][]string{
+		{
+			"",
+			"",
+			"",
+		},
+		{
+			"",
+			"",
+			"",
+		},
+		{
+			"",
+			"",
+			"",
+		},
+	}
+	// exam preparation tips
+	tips := [][]string{
+		{
+			"",
+			"",
+			"",
+		},
+		{
+			"",
+			"",
+			"",
+		},
+		{
+			"",
+			"",
+			"",
+		},
+	}
+	
+
+}
